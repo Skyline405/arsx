587216f6a8dfbf7ffe1fd053e156a5bd2e97f27e
raul <ranufriev@ac-sw.com>
2024-07-25 18:16:55 +0700

    refactor(network): minor refactor of concept

--- packages/network/src/lib/core/NetworkClient.ts
+++ packages/network/src/lib/core/NetworkClient.ts
@@ -1,4 +1,4 @@
-import { share } from "rxjs"
+import { defer, share } from "rxjs"
 import { NetworkContext } from "./NetworkContext"
 import { NetworkHandler } from "./NetworkHandler"
 import { NetworkStream } from "./NetworkStream"
@@ -8,11 +8,15 @@ export class NetworkClient<I, O> {
     private readonly handler: NetworkHandler<I, O>,
   ) {}
 
+  protected _buildRequest(request: I): I {
+    return request
+  }
+
   handle(
     request: I,
     context: NetworkContext = new NetworkContext(),
   ): NetworkStream<O> {
-    return this.handler(context)(request)
+    return defer(() => this.handler(context)(this._buildRequest(request)))
       .pipe(share())
   }
 }
--- packages/network/src/lib/core/NetworkHandler.ts
+++ packages/network/src/lib/core/NetworkHandler.ts
@@ -6,8 +6,10 @@ export type NetworkHandler<I, O> =
   (context: NetworkContext) => NetworkHandlerDelegate<I, O>
 
 // TODO: comeup with new name of this type
-export type NetworkMiddleware<I, O> =
-  (next: NetworkHandler<I, O>) => NetworkHandler<I, O>
+export type NetworkMiddleware<
+  A extends NetworkHandler<any, any>,
+  B extends NetworkHandler<any, any> = A
+> = (next: B) => A
 
 export type NetworkInterceptor<I, O> = (
   next: NetworkHandlerDelegate<I, O>,
@@ -15,20 +17,21 @@ export type NetworkInterceptor<I, O> = (
   context: NetworkContext,
 ) => NetworkStream<O>
 
-export const defineInterceptor = <I, O>(
-  interceptor: NetworkInterceptor<I, O>
-) => interceptor
-
 // TODO: use curry function
 const splitInterceptor = <I, O>(
   intercept: NetworkInterceptor<I, O>
-): NetworkMiddleware<I, O> =>
+): NetworkMiddleware<NetworkHandler<I, O>> =>
   (next) => (context) => (input) =>
     intercept(next(context), input, context)
 
+export const defineInterceptor = <I, O>(
+  interceptor: NetworkInterceptor<I, O>
+): NetworkMiddleware<NetworkHandler<I, O>> =>
+  splitInterceptor(interceptor)
+
 export const withInterceptors = <I, O>(
-  list: NetworkInterceptor<I, O>[],
+  list: NetworkMiddleware<NetworkHandler<I, O>>[],
   backend: NetworkHandler<I, O>,
 ): NetworkHandler<I, O> =>
-  list.reduceRight((handlerBuilder, interceptor) =>
-    splitInterceptor(interceptor)(handlerBuilder), backend)
+  list.reduceRight((handler, intercept) =>
+    intercept(handler), backend)
